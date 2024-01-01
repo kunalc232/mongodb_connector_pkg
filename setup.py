210966d11669bee0ef3fae9a8a99fc1e1cf5821f
kunalc232 <kunalc232@gmail.com>
2024-01-01 17:18:27 +0530

    deleted readme

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from typing import List
 
-with open('README.MD', 'r', encoding='utf-8') as f:
+with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
